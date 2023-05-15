e8b2b71ad158f59a401af6a8de5fd7618a025d38
roschool <roschool@rosschool>
2023-05-15 15:53:37 +0900

    폰팔기 프로그램

---
+++ java20409/src/s0515/Add.java
@@ -0,0 +1,18 @@
+package s0515;
+
+public class Add {
+	//오버로딩
+	//숫자더하기 add
+	int add(int a, int b) {
+		return a + b;
+	}
+	//문자 더하기 add
+	String add(String a, String b) {
+		
+		return a+b;
+	}
+	String add(String a, String b, String c) {
+		
+		return a+b+c;
+	}
+}
